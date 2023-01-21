dec9442bc794897c952beb34a6d57d1276278e4e
Prathmesh <pcshedge@gmail.com>
2023-01-21 15:04:53 +0530

    Solution

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -3,11 +3,14 @@ package com.driver;
 public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
+        super(name,isManual);
+
         //Use arbitrary values for parameters which are not mentioned
     }
 
     public void accelerate(int rate){
         int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
+        newSpeed = this.getCurrentSpeed()+rate;
         /**
          * speed 0: gear 1
          * speed 1-50: gear 1
@@ -19,6 +22,8 @@ public class F1 extends Car {
          */
 
         if(newSpeed == 0) {
+            this.stop();
+            this.changeGear(1);
             //Stop the car, set gear as 1
         }
         //for all other cases, change the gear accordingly
@@ -26,5 +31,30 @@ public class F1 extends Car {
         if(newSpeed > 0) {
             changeSpeed(newSpeed, getCurrentDirection());
         }
+        if(newSpeed>=1 && newSpeed<=50){
+           // changeSpeed(newSpeed,getCurrentDirection());
+            changeGear(1);
+        }
+        if(newSpeed>=51 && newSpeed<=100){
+            //changeSpeed(newSpeed,getCurrentDirection());
+            changeGear(2);
+        }
+        if(newSpeed>=101 && newSpeed<=150){
+            //changeSpeed(newSpeed,getCurrentDirection());
+            changeGear(3);
+        }
+        if(newSpeed>=151 && newSpeed<=200){
+            //changeSpeed(newSpeed,getCurrentDirection());
+            changeGear(4);
+        }
+        if(newSpeed>=201 && newSpeed<=250){
+            //changeSpeed(newSpeed,getCurrentDirection());
+            changeGear(5);
+        }
+        if(newSpeed>250){
+            //changeSpeed(newSpeed,getCurrentDirection());
+            changeGear(6);
+        }
+
     }
 }
