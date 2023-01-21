447ed6cd50f54f848542d252a63389ae8cf33a51
Prathmesh <pcshedge@gmail.com>
2023-01-21 16:10:21 +0530

    Solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -7,31 +7,6 @@ public class Main {
         mercedes.accelerate(20);
         mercedes.accelerate(-60);
 
-        Car car = new Car("Tata", 4, 4, 4, true, "Sedan");
-
-        assert car.getName() == "Tata";
-
-        assert car.getDoors() == 4;
-
-        assert car.getGears() == 4;
-
-        assert car.getWheels() == 4;
-
-        assert car.isManual();
-
-        assert car.getType() == "Sedan";
-
-        assert car.getCurrentGear() == 1;
-
-        car.changeSpeed(20, 30);
-
-        assert car.getCurrentSpeed() == 20;
-
-        assert car.getCurrentDirection() == 30;
-
-        car.changeGear(3);
-
-        assert car.getCurrentGear() == 3;
 
     }
 }
