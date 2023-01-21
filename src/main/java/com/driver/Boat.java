ebbf1f1a80b3a9454df25f89286a1dabe5700891
Prathmesh <pcshedge@gmail.com>
2023-01-21 15:39:50 +0530

    Solution3

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -3,14 +3,16 @@ package com.driver;
 public class Boat implements WaterVehicle {
     String name;
     int capacity;
+
     public Boat() {
     }
-    @Override
+
+
     public String getVehicleName() {
         return this.name;
     }
 
-    @Override
+
     public int getVehicleCapacity() {
         return this.capacity;
     }
